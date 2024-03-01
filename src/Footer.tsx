3b1e83bfd8cd289ad2c2aab883354657f43b2a41
ToTo Antonio <totoantonio@gmail.com>
2024-03-01 22:38:02 +0800

    pratice react

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-1 no-border">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-1">
         <div className="col-md-4 d-flex align-items-center">
           <a
             href="/"
