ab12cc1c031e309af3cf52bd6ee561a26f959567
ToTo Antonio <totoantonio@gmail.com>
2024-03-02 13:19:09 +0800

    changes

--- src/ZodiacFinder.tsx
+++ src/ZodiacFinder.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./mycss.css";
+import "./mycss.css"; // Import your custom CSS styles here
 import * as zodiacData from "./chinesezodiac.json";
+import axios from "axios"; // Import Axios for making HTTP requests
 
 interface ZodiacInfo {
   description: string;
@@ -12,20 +13,45 @@ interface ZodiacData {
   [key: string]: ZodiacInfo;
 }
 
+interface Horoscope {
+  horoscope: string;
+}
+
 interface ZodiacFinderProps {
   birthYear?: string | null;
 }
+
+const titleStyle: CustomCSSProperties = {
+  "--gradientColor": "linear-gradient(89deg, #2689E8 18.91%, #A168FF 79.91%)",
+};
+
+interface CustomCSSProperties extends React.CSSProperties {
+  "--gradientColor": string;
+}
+
+const getCurrentDate = () => {
+  const dateObj = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return dateObj.toLocaleDateString("en-US", options);
+};
 
 const ZodiacFinder: React.FC<ZodiacFinderProps> = ({ birthYear }) => {
   const [showResult, setShowResult] = useState(false);
   const [zodiacSign, setZodiacSign] = useState("");
   const [zodiacDescription, setZodiacDescription] = useState("");
   const [luckyNumbers, setLuckyNumbers] = useState<number[]>([]);
+  const [horoscope, setHoroscope] = useState("");
   const resultRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (birthYear) {
       calculateZodiac(birthYear);
+      fetchHoroscope();
     }
   }, [birthYear]);
 
@@ -62,17 +88,6 @@ const ZodiacFinder: React.FC<ZodiacFinderProps> = ({ birthYear }) => {
       setZodiacDescription(zodiacInfo.description);
       setLuckyNumbers(generateLuckyNumbers(7));
       setShowResult(true);
-
-      // Scroll to the result section
-      console.log("Scrolling to result section:", resultRef.current);
-      setTimeout(() => {
-        if (resultRef.current) {
-          resultRef.current.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          });
-        }
-      }, 100); // Adjust timeout as needed
     }
   };
 
@@ -87,45 +102,79 @@ const ZodiacFinder: React.FC<ZodiacFinderProps> = ({ birthYear }) => {
     return numbers;
   };
 
-  const handleToggleResult = () => {
-    setShowResult(false); // Reset showResult state
-    // Other logic to trigger calculation of new result
+  const fetchHoroscope = async () => {
+    try {
+      const response = await axios.get<Horoscope>(
+        "YOUR_HOROSCOPE_API_ENDPOINT"
+      );
+      setHoroscope(response.data.horoscope);
+    } catch (error) {
+      console.error("Error fetching horoscope:", error);
+    }
   };
 
   return (
     <div className={`container mt-5 ${showResult ? "" : "hidden"}`}>
       <div ref={resultRef} className="result-section">
         {showResult && (
-          <div className="modal-content rounded-4 shadow">
-            <div className="modal-body p-2">
-              <span className="display-5 fw-bolder zodiactitle mb-0">
-                {zodiacSign}
-              </span>
-              <p className="lead lh-1 p-3">{zodiacDescription}</p>
-              <p className="lead lh-1 p-3 pt-4">
-                Your lucky numbers:
-                <br />
-                <br />
-                <span className="lucky-numbers-text">
-                  {luckyNumbers.map((number, index) => (
-                    <span key={index} className="lucky-number">
-                      {number}
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="modal-content rounded-4 shadow">
+                <div className="modal-body p-2">
+                  <h1 className="display-5 fw-bold text-body-emphasis zodiactitle mb-3">
+                    <span className="gradient-text" style={titleStyle}>
+                      {" "}
+                      {zodiacSign}
                     </span>
-                  ))}
-                </span>
-              </p>
+                  </h1>
 
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 gap-3"
-                data-bs-dismiss="modal"
-                onClick={() => {
-                  setShowResult(false);
-                  window.scrollTo({ top: 0, behavior: "smooth" });
-                }}
-              >
-                Great, thanks!
-              </button>
+                  <p className="lead lh-1 p-3">{zodiacDescription}</p>
+                  <p className="lead lh-1 p-3 pt-4">
+                    Your lucky numbers:
+                    <br />
+                    <br />
+                    <span className="lucky-numbers-text gradient-text">
+                      {luckyNumbers.map((number, index) => (
+                        <span key={index} className="lucky-number">
+                          {number}
+                        </span>
+                      ))}
+                    </span>
+                  </p>
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg px-4 gap-3"
+                    data-bs-dismiss="modal"
+                    onClick={() => {
+                      setShowResult(false);
+                      window.scrollTo({ top: 0, behavior: "smooth" });
+                    }}
+                  >
+                    Great, thanks!
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="modal-content rounded-4 shadow">
+                <div className="modal-body p-2">
+                  <h1 className="display-5 fw-bold text-body-emphasis zodiactitle mb-3">
+                    <span className="gradient-text" style={titleStyle}>
+                      {" "}
+                      {zodiacSign}
+                    </span>
+                  </h1>
+
+                  <small>{getCurrentDate()}</small>
+                  <p className="lead lh-1 p-3">{horoscope}</p>
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg px-4 me-md-2"
+                  >
+                    Primary
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         )}
