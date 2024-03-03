03a811207cb0ac16d6c9a762b6ec2014709d96ae
ToTo Antonio <totoantonio@gmail.com>
2024-03-03 19:15:17 +0800

    changes

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const title = "TLG";
 
 function NavBar() {
-  const [isPulsating, setIsPulsating] = useState(false);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIsPulsating((prev) => !prev); // Toggle pulsation state
-    }, 1000); // Pulsate every 1000ms (1 second)
-
-    return () => clearInterval(intervalId); // Cleanup for the interval effect
-  }, []);
   return (
     <nav className="navbar bg-white fs-5 fw-bold fs-4">
       <div className="container-fluid">
@@ -33,4 +24,5 @@ function NavBar() {
     </nav>
   );
 }
+
 export default NavBar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import NavBar from "./NavBar";
 import Title from "./Title";
 import ZodiacFinder from "./ZodiacFinder";
 import Footer from "./Footer";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <NavBar />
     <Title />
     <ZodiacFinder />
     <Footer />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
