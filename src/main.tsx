c23f496aceaf48f67c44bcc399de2d0e1900e7d0
ToTo Antonio <totoantonio@gmail.com>
2024-03-04 08:55:37 +0800

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import NavBar from "./NavBar";
 import Title from "./Title";
 import ZodiacFinder from "./ZodiacFinder";
 import Footer from "./Footer";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as Element;
+createRoot(rootElement).render(
   <React.StrictMode>
     <NavBar />
     <Title />
     <ZodiacFinder />
     <Footer />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
