c362e167038e69299f6ccae44a7130fe6700da9d
ToTo Antonio <totoantonio@gmail.com>
2024-03-30 22:44:58 +0800

    changes

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -13,7 +13,6 @@ interface FooterProps {
 }
 
 const Footer: React.FC<FooterProps> = ({ isLightTheme }) => {
-  const textClass = isLightTheme ? "text-dark" : "text-white";
   const [showCookieWarning, setShowCookieWarning] = useState(false);
   const cookieWarningRef = useRef<HTMLDivElement>(null);
 
@@ -59,49 +58,64 @@ const Footer: React.FC<FooterProps> = ({ isLightTheme }) => {
     };
   }, []);
 
-  const CookieWarning = () => (
-    <div
-      className={`cookie-warning-card ${textClass}`}
-      style={{
-        display: showCookieWarning ? "flex" : "none",
-        position: "absolute", // Corrected
-        right: "20px",
-        bottom: "40px",
-        maxWidth: "400px",
-        padding: "10px",
-        backgroundColor: "#dee2e6",
-        borderRadius: "10px",
-        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-        zIndex: 9999,
-      }}
-      ref={cookieWarningRef}
-    >
+  const CookieWarning = () => {
+    const cookieWarningClass = showCookieWarning ? "show" : "";
+
+    return (
       <div
-        className="lh-1 d-flex justify-content-between align-items-center"
-        style={{ width: "100%" }}
+        className={`cookie-warning-card ${cookieWarningClass}`}
+        style={{
+          display: showCookieWarning ? "flex" : "none",
+          position: "absolute",
+          right: "20px",
+          bottom: "40px",
+          maxWidth: "400px",
+          padding: "10px",
+          backgroundColor: isLightTheme ? "#dee2e6" : "#333", // Change background color based on theme
+          borderRadius: "10px",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+          zIndex: 9999,
+          color: isLightTheme ? "black" : "white", // Adjust text color based on theme
+        }}
+        ref={cookieWarningRef}
       >
-        <div className="lh-1 d-flex align-items-center" style={{ flex: "1" }}>
-          <img
-            src="cookie.svg"
-            alt="Cookie"
-            style={{ width: "30px", height: "30px", marginRight: "10px" }}
-          />
-        </div>
-        <div style={{ flex: "5", marginLeft: "5px", fontSize: "16px" }}>
-          This website uses cookies. Awesome, Right? This website also uses a
-          flickering image in our header. Stay away if you have neuro
-          conditions.
-        </div>
-        <button
-          className={`btn btn-link ${textClass}`}
-          onClick={handleCloseCookieWarning}
-          style={{ flex: "none", boxShadow: "none", border: "none" }}
+        <div
+          className="lh-1 d-flex justify-content-between align-items-center"
+          style={{ width: "100%" }}
         >
-          <FaTimes style={{ color: "black" }} />
-        </button>
+          <div className="lh-1 d-flex align-items-center" style={{ flex: "1" }}>
+            <img
+              src="cookie.svg"
+              alt="Cookie"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginRight: "10px",
+                filter: isLightTheme ? "none" : "invert(1)",
+              }}
+            />
+          </div>
+          <div style={{ flex: "5", marginLeft: "5px", fontSize: "16px" }}>
+            This website uses cookies. Awesome, Right? This website also uses a
+            flickering image in our header. Stay away if you have neuro
+            conditions.
+          </div>
+          <button
+            className={`btn btn-link`}
+            onClick={handleCloseCookieWarning}
+            style={{
+              flex: "none",
+              boxShadow: "none",
+              border: "none",
+              color: isLightTheme ? "black" : "white", // Adjust button text color based on theme
+            }}
+          >
+            <FaTimes />
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   return (
     <footer
@@ -119,13 +133,27 @@ const Footer: React.FC<FooterProps> = ({ isLightTheme }) => {
             />
           </LazyLoad>
         </a>
-        <span className={`card-text ${textClass}`}>&copy; 2024 O2, Inc</span>
+        <span
+          className={`card-text ${isLightTheme ? "text-dark" : "text-white"}`}
+        >
+          &copy; 2024 O2, Inc
+        </span>
       </div>
       <div className="d-flex align-items-center">
-        <a className={`me-3 text-body-secondary ${textClass}`} href="#">
+        <a
+          className={`me-3 text-body-secondary ${
+            isLightTheme ? "text-dark" : "text-white"
+          }`}
+          href="#"
+        >
           <FaTwitter size={24} style={isLightTheme ? {} : { color: "white" }} />
         </a>
-        <a className={`me-3 text-body-secondary ${textClass}`} href="#">
+        <a
+          className={`me-3 text-body-secondary ${
+            isLightTheme ? "text-dark" : "text-white"
+          }`}
+          href="#"
+        >
           <FaInstagram
             size={24}
             style={isLightTheme ? {} : { color: "white" }}
