fb0578a040b0c29a66d0f88b0ddf4b8ef2c505db
ToTo Antonio <totoantonio@gmail.com>
2024-03-30 16:15:48 +0800

    changes

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BiMoon, BiSun, BiMenu } from "react-icons/bi";
 import { FaReact, FaLeaf, FaGithub, FaTelegram } from "react-icons/fa";
+import FocusTrap from "focus-trap-react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./mycss.css";
+import "./mycss.css"; // Make sure you have this file for custom styles
 
 interface NavBarProps {
   isLightTheme: boolean;
@@ -12,25 +13,18 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({ isLightTheme, toggleTheme }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (isModalVisible) {
-      disableScrolling();
+      document.body.classList.add("modal-open");
     } else {
-      enableScrolling();
+      document.body.classList.remove("modal-open");
     }
   }, [isModalVisible]);
 
   const toggleModal = () => {
     setIsModalVisible((prev) => !prev);
-  };
-
-  const disableScrolling = () => {
-    document.body.style.overflow = "hidden";
-  };
-
-  const enableScrolling = () => {
-    document.body.style.overflow = "unset";
   };
 
   const SunIcon = <BiSun />;
@@ -46,7 +40,7 @@ const NavBar: React.FC<NavBarProps> = ({ isLightTheme, toggleTheme }) => {
       <nav className={`navbar ${navbarColorClass}`}>
         <div className="container-fluid">
           <button
-            className={`navbar-toggler`}
+            className="navbar-toggler"
             type="button"
             onClick={toggleModal}
             aria-label="Toggle navigation"
@@ -71,94 +65,95 @@ const NavBar: React.FC<NavBarProps> = ({ isLightTheme, toggleTheme }) => {
           className={`modal ${isLightTheme ? "text-dark" : "text-light"}`}
           tabIndex={-1}
           role="dialog"
-          style={{
-            display: "block",
-            backgroundColor: isLightTheme
-              ? "rgba(255, 255, 255, 0.5)"
-              : "rgba(0, 0, 0, 0.5)",
-          }}
+          style={{ display: "block" }}
+          ref={modalRef}
         >
-          <div className="modal-dialog" role="document">
-            <div
-              className={`modal-content border-0 ${
-                isLightTheme ? "bg-white" : "bg-dark"
-              }`}
-            >
-              <div className="modal-header">
-                <h5
-                  className={`modal-title ${
-                    isLightTheme ? "text-dark" : "text-light"
-                  }`}
-                >
-                  About Us
-                </h5>
-                <button
-                  type="button"
-                  className={`btn-close ${
-                    isLightTheme ? "btn-light" : "btn-dark"
-                  }`}
-                  onClick={toggleModal}
-                  aria-label="Close"
-                  style={{
-                    border: "none", // Remove any border
-                    background: "none", // Remove background
-                    cursor: "pointer", // Add pointer cursor for better usability
-                    position: "relative", // Ensure button is positioned relative
-                  }}
-                >
-                  {/* SVG icon for the close button */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill={isLightTheme ? "black" : "white"}
-                    viewBox="0 0 16 16"
+          <FocusTrap focusTrapOptions={{ initialFocus: ".modal-content" }}>
+            <div className="modal-dialog" role="document">
+              <div
+                className={`modal-content border-0 ${
+                  isLightTheme ? "bg-white" : "bg-dark"
+                }`}
+              >
+                <div className="modal-header">
+                  <h5
+                    className={`modal-title ${
+                      isLightTheme ? "text-dark" : "text-light"
+                    }`}
+                  >
+                    About Us
+                  </h5>
+                  <button
+                    type="button"
+                    className={`btn-close ${
+                      isLightTheme ? "btn-light" : "btn-dark"
+                    }`}
+                    onClick={toggleModal}
+                    aria-label="Close"
                     style={{
-                      position: "absolute", // Position the SVG icon absolutely
-                      top: "50%", // Center vertically
-                      left: "50%", // Center horizontally
-                      transform: "translate(-50%, -50%)", // Center the SVG icon
+                      border: "none !important",
+                      background: "none",
+                      cursor: "pointer",
+                      position: "absolute",
+                      top: "20px",
+                      right: "20px",
                     }}
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.854 3.146a.5.5 0 0 1 .708 0L8 7.293l3.646-3.647a.5.5 0 1 1 .708.708L8.707 8l3.647 3.646a.5.5 0 1 1-.708.708L8 8.707l-3.646 3.647a.5.5 0 0 1-.708-.708L7.293 8 3.646 4.354a.5.5 0 0 1 0-.708z"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="modal-body lh-sm">
-                <p>
-                  Welcome to <strong>Luck Generator - Find Your Luck</strong>.
-                  I'm a hobbyist web developer who enjoys building with React
-                  and Vite.
-                </p>
-                <p>
-                  This website is hosted on GitHub, where I occasionally update
-                  it during my free time or when I surpass my sales targets (my
-                  day job).
-                </p>
-                <p>
-                  I'm a firm believer in luck and its role in our lives. Join me
-                  as we explore how technology and luck intersect.
-                </p>
-                <p>
-                  Still working on our Light and Dark Theme Mode. I am not sure
-                  when i can finish it. But notice that it is already working in
-                  our NavBar.
-                </p>
-              </div>
-              <div className="d-flex justify-content-between align-items-center px-4">
-                <FaReact size={32} color={isLightTheme ? "black" : "white"} />
-                <FaLeaf size={32} color={isLightTheme ? "black" : "white"} />
-                <FaGithub size={32} color={isLightTheme ? "black" : "white"} />
-                <FaTelegram
-                  size={32}
-                  color={isLightTheme ? "black" : "white"}
-                />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill={isLightTheme ? "black" : "white"}
+                      viewBox="0 0 16 16"
+                      style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                      }}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3.854 3.146a.5.5 0 0 1 .708 0L8 7.293l3.646-3.647a.5.5 0 1 1 .708.708L8.707 8l3.647 3.646a.5.5 0 1 1-.708.708L8 8.707l-3.646 3.647a.5.5 0 0 1-.708-.708L7.293 8 3.646 4.354a.5.5 0 0 1 0-.708z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div className="modal-body lh-sm">
+                  <p>
+                    Welcome to<strong>Luck Generator - Find Your Luck</strong>.
+                    I'm a hobbyist web developer who enjoys building with React
+                    and Vite.
+                  </p>
+                  <p>
+                    This website is hosted on GitHub, where I occasionally
+                    update it during my free time.
+                  </p>
+                  <p>
+                    I'm a firm believer in luck and its role in our lives. Join
+                    me as we explore how technology and luck intersect.
+                  </p>
+                  <p>
+                    Still working on our Light and Dark Theme Mode. I am not
+                    sure when i can finish it. But notice that it is already
+                    working in our NavBar.
+                  </p>
+                </div>
+                <div className="d-flex justify-content-between align-items-center px-4">
+                  <FaReact size={32} color={isLightTheme ? "black" : "white"} />
+                  <FaLeaf size={32} color={isLightTheme ? "black" : "white"} />
+                  <FaGithub
+                    size={32}
+                    color={isLightTheme ? "black" : "white"}
+                  />
+                  <FaTelegram
+                    size={32}
+                    color={isLightTheme ? "black" : "white"}
+                  />
+                </div>
               </div>
             </div>
-          </div>
+          </FocusTrap>
         </div>
       )}
     </>
--- src/blog.tsx
+++ src/blog.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import LazyLoad from "react-lazyload";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 interface BlogProps {
@@ -7,70 +6,119 @@ interface BlogProps {
 }
 
 const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
-  const textClass = isLightTheme ? "text-white" : "text-dark";
-  const bgColorClass = isLightTheme ? "bg-dark" : "bg-light";
+  const textClass = isLightTheme ? "text-dark" : "text-white";
+  const bgColorClass = isLightTheme ? "bg-light" : "bg-dark";
 
   return (
-    <div className="container px-4 pt-5">
-      <div
-        className={`box-shadow py-2 px-4 py-md-2 px-md-4 overflow-hidden rounded-3 h-100 row justify-content-center align-items-center ${bgColorClass}`}
-      >
-        <div className="col-lg-8">
-          <p className={`display-6 fw-bold mb-3 lh-1 pt-3 pb-3 ${textClass}`}>
-            Unlocking the Mystery: <br />
-            Lucky Numbers Across Cultures <br />
-          </p>
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            China: In Chinese culture, the number 8 is considered extremely
-            lucky because its pronunciation sounds similar to the word for
-            "wealth" or "prosper." Similarly, the number 6 is considered lucky
-            as it sounds like the word for "smooth" or "well-off." Conversely,
-            the number 4 is avoided as it sounds similar to the word for
-            "death."
-          </p>
-
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            Japan: In Japan, the number 7 is considered lucky, stemming from its
-            association with the Seven Gods of Fortune. Additionally, the number
-            9 is considered lucky because it's pronounced similarly to the word
-            for "long-lasting."
-          </p>
-
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            India: In Hindu culture, the number 9 is considered auspicious and
-            is associated with the goddess of wealth, Lakshmi. It's also common
-            for numbers ending in 1 to be considered lucky.
-          </p>
-
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            United States: In Western culture, the number 7 is often regarded as
-            lucky, influenced by its significance in religious texts and
-            folklore. It's associated with good fortune and perfection.
-            Additionally, the number 13 is often considered unlucky, leading to
-            the superstition of Friday the 13th.
-          </p>
-
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            Islamic cultures: In Islamic cultures, the number 786 is considered
-            lucky because it represents the phrase "Bismillah al-Rahman
-            al-Rahim" (In the name of Allah, the Most Gracious, the Most
-            Merciful) when using Arabic numerals.
-          </p>
+    <div className={`container px-4 pt-5 ${bgColorClass}`}>
+      <div className="row">
+        <div className="col-12">
+          <h2 className={`text-center pt-5 pb-3 ${textClass}`}>
+            Unlocking the Mystery: Lucky Numbers Across Cultures
+          </h2>
         </div>
-        <div className="col-lg-8">
-          <p className={`display-6 fw-bold mb-3 lh-1 ${textClass}`}>
-            The Power of Positive Thinking: Harnessing Optimism for Success{" "}
-            <br />
-          </p>
-          <p className={`lh-sm text-start pb-3 ${textClass}`}>
-            Positive thinking is a mindset that emphasizes focusing on the good,
-            expecting favorable outcomes, and maintaining an optimistic
-            attitude, even in challenging situations. This approach can lead to
-            numerous benefits, including improved mental health, increased
-            resilience, and enhanced problem-solving abilities. By cultivating
-            positivity in your thoughts and actions, you can pave the way for
-            greater success and fulfillment in life.
-          </p>
+      </div>
+      <div className="row">
+        {/* First Row */}
+        <div className="col-lg-4">
+          <div className="text-center">
+            <img
+              src="./cultures.avif"
+              alt="Culture 1"
+              className="img-fluid rounded-circle border border-white mb-3"
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+            <p
+              className={`text-start lh-1 ${textClass}`}
+              style={{ fontSize: "16px" }}
+            >
+              <strong>China:</strong> In Chinese culture, the number 8 is
+              considered extremely lucky because its pronunciation sounds
+              similar to the word for "wealth" or "prosper." Similarly, the
+              number 6 is considered lucky as it sounds like the word for
+              "smooth" or "well-off." Conversely, the number 4 is avoided as it
+              sounds similar to the word for "death."
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="text-center">
+            <img
+              src="./cultureJapan.avif"
+              alt="Culture 2"
+              className="img-fluid rounded-circle border border-white mb-3"
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+            <p
+              className={`text-start lh-1 ${textClass}`}
+              style={{ fontSize: "16px" }}
+            >
+              <strong>Japan:</strong> In Japan, the number 7 is considered
+              lucky, stemming from its association with the Seven Gods of
+              Fortune. Additionally, the number 9 is considered lucky because
+              it's pronounced similarly to the word for "long-lasting."
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="text-center">
+            <img
+              src="./cultureIndia.avif"
+              alt="Culture 3"
+              className="img-fluid rounded-circle border border-white mb-3"
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+            <p
+              className={`text-start lh-1 ${textClass}`}
+              style={{ fontSize: "16px" }}
+            >
+              <strong>India:</strong> In Hindu culture, the number 9 is
+              considered auspicious and is associated with the goddess of
+              wealth, Lakshmi. It's also common for numbers ending in 1 to be
+              considered lucky.
+            </p>
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="col-lg-4">
+          <div className="text-center">
+            <img
+              src="./cultureWest.avif"
+              alt="Culture 4"
+              className="img-fluid rounded-circle border border-white mb-3"
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+            <p
+              className={`text-start lh-1 ${textClass}`}
+              style={{ fontSize: "16px" }}
+            >
+              <strong>United States:</strong> In Western culture, the number 7
+              is often regarded as lucky, influenced by its significance in
+              religious texts and folklore. It's associated with good fortune
+              and perfection. Additionally, the number 13 is often considered
+              unlucky, leading to the superstition of Friday the 13th.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="text-center">
+            <img
+              src="./cultureEast.avif"
+              alt="Culture 5"
+              className="img-fluid rounded-circle border border-white mb-3"
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+            <p
+              className={`text-start lh-1 ${textClass}`}
+              style={{ fontSize: "16px" }}
+            >
+              <strong>Islamic cultures:</strong> In Islamic cultures, the number
+              786 is considered lucky because it represents the phrase
+              "Bismillah al-Rahman al-Rahim" (In the name of Allah, the Most
+              Gracious, the Most Merciful) when using Arabic numerals.
+            </p>
+          </div>
         </div>
       </div>
     </div>
