8cccb819b13d24417e2f4308c135bce0dc324a8a
ToTo Antonio <totoantonio@gmail.com>
2024-03-31 23:28:38 +0800

    changes

--- src/blog.tsx
+++ src/blog.tsx
@@ -11,10 +11,10 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
   const bgColorClass = isLightTheme ? "bg-light" : "bg-dark";
 
   return (
-    <div className={`container px-4 pt-5 ${bgColorClass}`}>
+    <div className={`container px-4 pt-3 ${bgColorClass}`}>
       <div className="row">
         <div className="col-12">
-          <h2 className={`text-center pt-5 pb-3 ${textClass}`}>
+          <h2 className={`lh-1 text-center pt-3 pb-3 ${textClass}`}>
             Unlocking the Mystery: Lucky Numbers Across Cultures
           </h2>
         </div>
@@ -32,12 +32,13 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
               />
             </LazyLoad>
             <p className={`text-start lh-1 ${textClass}`}>
-              <strong>China:</strong> In Chinese culture, the number 8 is
-              considered extremely lucky because its pronunciation sounds
-              similar to the word for "wealth" or "prosper." Similarly, the
-              number 6 is considered lucky as it sounds like the word for
-              "smooth" or "well-off." Conversely, the number 4 is avoided as it
-              sounds similar to the word for "death."
+              <strong>China:</strong> In Chinese culture, the number{" "}
+              <strong>8</strong> is considered extremely lucky because its
+              pronunciation sounds similar to the word for "wealth" or
+              "prosper." Similarly, the number <strong>6</strong> is considered
+              lucky as it sounds like the word for "smooth" or "well-off."
+              Conversely, the number <strong>4</strong> is avoided as it sounds
+              similar to the word for "death."
             </p>
           </div>
         </div>
@@ -52,10 +53,10 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
               />
             </LazyLoad>
             <p className={`text-start lh-1 ${textClass}`}>
-              <strong>Japan:</strong> In Japan, the number 7 is considered
-              lucky, stemming from its association with the Seven Gods of
-              Fortune. Additionally, the number 9 is considered lucky because
-              it's pronounced similarly to the word for "long-lasting."
+              <strong>Japan:</strong> In Japan, the number <strong>7</strong> is
+              considered lucky, stemming from its association with the Seven
+              Gods of Fortune. Additionally, the number 9 is considered lucky
+              because it's pronounced similarly to the word for "long-lasting."
             </p>
           </div>
         </div>
@@ -70,10 +71,10 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
               />
             </LazyLoad>
             <p className={`text-start lh-1 ${textClass}`}>
-              <strong>India:</strong> In Hindu culture, the number 9 is
-              considered auspicious and is associated with the goddess of
-              wealth, Lakshmi. It's also common for numbers ending in 1 to be
-              considered lucky.
+              <strong>India:</strong> In Hindu culture, the number{" "}
+              <strong>9</strong> is considered auspicious and is associated with
+              the goddess of wealth, Lakshmi. It's also common for numbers
+              ending in 1 to be considered lucky.
             </p>
           </div>
         </div>
@@ -90,11 +91,13 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
               />
             </LazyLoad>
             <p className={`text-start lh-1 ${textClass}`}>
-              <strong>United States:</strong> In Western culture, the number 7
+              <strong>United States:</strong> In Western culture, the number{" "}
+              <strong>7</strong>
               is often regarded as lucky, influenced by its significance in
               religious texts and folklore. It's associated with good fortune
-              and perfection. Additionally, the number 13 is often considered
-              unlucky, leading to the superstition of Friday the 13th.
+              and perfection. Additionally, the number <strong>13</strong> is
+              often considered unlucky, leading to the superstition of Friday
+              the 13th.
             </p>
           </div>
         </div>
@@ -110,9 +113,9 @@ const Blog: React.FC<BlogProps> = ({ isLightTheme }) => {
             </LazyLoad>
             <p className={`text-start lh-1 ${textClass}`}>
               <strong>Islamic cultures:</strong> In Islamic cultures, the number
-              786 is considered lucky because it represents the phrase
-              "Bismillah al-Rahman al-Rahim" (In the name of Allah, the Most
-              Gracious, the Most Merciful) when using Arabic numerals.
+              <strong>786</strong> is considered lucky because it represents the
+              phrase "Bismillah al-Rahman al-Rahim" (In the name of Allah, the
+              Most Gracious, the Most Merciful) when using Arabic numerals.
             </p>
           </div>
         </div>
